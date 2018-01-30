4da39d930c5375f1019e58712b29047f4b5901df
wkhola <wk_hola@163.com>
2018-01-30 22:02:27 +0800

    flask_echarts

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import MySQLdb
 # app入口定义
 app = Flask(__name__)
 #链接数据库
-conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='root',db='ca')
+conn=MySQLdb.connect(host='127.0.0.1',user='root',passwd='123456',db='ca2')
 #建立操作游标
 cursor=conn.cursor()
 #设置数据输入输出编码格式
--- index.py
+++ index.py
@@ -1,19 +1,23 @@
 # coding:utf-8
 # 引入app配置
-from flask import jsonify  # JSON
-from flask import render_template #读取页面
-from application import app,cursor#引入app和cursor
+from flask import jsonify  # 把列表 序列转换成JSON
+from flask import render_template  # 读取页面
+from application import app, cursor  # 引入app和cursor
 
-#首页
+
+# 首页
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#地点薪资图页面(地图)
+
+# 地点薪资图页面(地图)
 # 图表形式参考链接：http://echarts.baidu.com/demo.html#map-china-dataRange
 @app.route('/zwyx/dd_index')
 def zwyx_dd_view():
     return render_template('zwyx_dd.html')
+
+
 # 地点和薪资
 @app.route('/zwyx/dd')
 def show_zwyx_dd():
@@ -21,52 +25,56 @@ def show_zwyx_dd():
     returnDate = {}
     returnDate['status'] = 0
     # 查询地点和薪资的关系，职位总数，平均薪资
-    sql='select count(ca_list.Id) as count_zw,avg(ca_list.zwyx) as avg_zwyx,ca_dd.province from ca_list inner join ca_dd on ca_dd.Id=ca_list.dd_id group by dd_id order by count_zw desc'
-    #执行sql语句
+    sql = 'select count(ca_list.Id) as count_zw,avg(ca_list.zwyx) as avg_zwyx,ca_dd.province from ca_list inner join ca_dd on ca_dd.Id=ca_list.dd_id group by dd_id order by count_zw desc'
+    # 执行sql语句
     cursor.execute(sql)
-    #取出所有结果集
+    # 取出所有结果集
     dd_zwyx_list = cursor.fetchall()
-    #平均薪资
-    avg_zwyx={}
-    #总职位数
-    count_zw={}
+    # 平均薪资
+    avg_zwyx = {}
+    # 总职位数
+    count_zw = {}
     if dd_zwyx_list:
-        #循环遍历重新构建数据
+        # 循环遍历重新构建数据
         for value in dd_zwyx_list:
-            #取得地点名
-            dd_name=value[2]
-            #判断总职位数是否存在地点名
+            # 取得地点名
+            dd_name = value[2]
+            # 判断总职位数是否存在地点名
             if not count_zw.has_key(dd_name):
-                #地点名不存在则创建记录并存入对应数值
-                count_zw[dd_name]={'name':dd_name,'value':value[0]}
+                # 地点名不存在则创建记录并存入对应数值
+                count_zw[dd_name] = {'name': dd_name, 'value': value[0]}
             else:
-                #存在则在原来基础上增加新的数值
-                count_zw[dd_name] = {'name': dd_name, 'value': (value[0]+count_zw[dd_name]['value'])}
+                # 存在则在原来基础上增加新的数值
+                count_zw[dd_name] = {'name': dd_name, 'value': (value[0] + count_zw[dd_name]['value'])}
             # 判断平均薪资是否存在地点名
             if not avg_zwyx.has_key(dd_name):
-                #不存在则创建记录并存入对应数值
-                avg_zwyx[dd_name] = {'name': dd_name, 'value': round(value[1],2)}
+                # 不存在则创建记录并存入对应数值
+                avg_zwyx[dd_name] = {'name': dd_name, 'value': round(value[1], 2)}
             else:
-                #存在则重新计算平均薪资
-                avg_zwyx[dd_name] = {'name': dd_name, 'value': round((round(value[1],2) + avg_zwyx[dd_name]['value'])/2,2)}
-        #重新构建数据
-        return_count_zw=[]
-        return_avg_zwyx=[]
+                # 存在则重新计算平均薪资
+                avg_zwyx[dd_name] = {'name': dd_name,
+                                     'value': round((round(value[1], 2) + avg_zwyx[dd_name]['value']) / 2, 2)}
+        # 重新构建数据
+        return_count_zw = []
+        return_avg_zwyx = []
         for item in count_zw:
             return_count_zw.append(count_zw[item])
         for item in avg_zwyx:
             return_avg_zwyx.append(avg_zwyx[item])
-        #数据汇总
-        returnDate['count_zw']=return_count_zw
+        # 数据汇总
+        returnDate['count_zw'] = return_count_zw
         returnDate['avg_zwyx'] = return_avg_zwyx
         returnDate['status'] = 1
     return jsonify(returnDate)
 
-#学历薪资图页面（柱状图）
+
+# 学历薪资图页面（柱状图）
 # 参考链接:http://echarts.baidu.com/demo.html#bar-stack
 @app.route('/zwyx/xl_index')
 def zwyx_xl_view():
     return render_template('zwyx_xl.html')
+
+
 # 学历和薪资
 @app.route('/zwyx/xl')
 def show_zwyx_xl():
@@ -81,24 +89,24 @@ def show_zwyx_xl():
     xl_zwyx_list = cursor.fetchall()
     if xl_zwyx_list:
         returnDate['status'] = 1
-        #总职位数
-        count_zw=[]
-        #平均薪资
-        avg_zw=[]
-        #学历名
-        xl_list=[]
-        #最大薪资
-        max_xz=[]
-        #最小薪资
-        min_xz=[]
-        #循环遍历存入数据
+        # 总职位数
+        count_zw = []
+        # 平均薪资
+        avg_zw = []
+        # 学历名
+        xl_list = []
+        # 最大薪资
+        max_xz = []
+        # 最小薪资
+        min_xz = []
+        # 循环遍历存入数据
         for item in xl_zwyx_list:
             count_zw.append(item[0])
-            avg_zw.append(round(item[1],2))#精度保留2位小数
+            avg_zw.append(round(item[1], 2))  # 精度保留2位小数
             xl_list.append(item[2])
             max_xz.append(item[3])
             min_xz.append(item[4])
-        #数据json化
+        # 数据json化
         returnDate['count_zw'] = count_zw
         returnDate['avg_zw'] = avg_zw
         returnDate['xl_list'] = xl_list
@@ -106,11 +114,14 @@ def show_zwyx_xl():
         returnDate['min_xz'] = min_xz
     return jsonify(returnDate)
 
-#公司规模薪资图页面（饼图）
+
+# 公司规模薪资图页面（饼图）
 # 参考链接：http://echarts.baidu.com/demo.html#pie-roseType
 @app.route('/zwyx/gsgm_index')
 def zwyx_gsgm_view():
     return render_template('zwyx_gsgm.html')
+
+
 # 公司规模和薪资（饼图）
 @app.route('/zwyx/gsgm')
 def show_zwyx_gsgm():
@@ -121,7 +132,7 @@ def show_zwyx_gsgm():
     # 执行sql语句
     cursor.execute(sql)
     # 取出所有结果集
-    gsgm_zwyx_list =cursor.fetchall()
+    gsgm_zwyx_list = cursor.fetchall()
     if gsgm_zwyx_list:
         returnDate['status'] = 1
         # 总职位数
@@ -132,19 +143,22 @@ def show_zwyx_gsgm():
         gsgm_list = []
         # 循环遍历存入数据
         for item in gsgm_zwyx_list:
-            count_zw.append({'name':item[2],'value':item[0]})
-            avg_zw.append({'name':item[2],'value':round(item[1],2)})
+            count_zw.append({'name': item[2], 'value': item[0]})
+            avg_zw.append({'name': item[2], 'value': round(item[1], 2)})
             gsgm_list.append(item[2])
         returnDate['count_zw'] = count_zw
         returnDate['avg_zw'] = avg_zw
         returnDate['gsgm_list'] = gsgm_list
     return jsonify(returnDate)
 
+
 # 公司性质薪资图页面（折线图）
 # 参考链接：http://echarts.baidu.com/demo.html#line-marker
 @app.route('/zwyx/gsxz_index')
 def zwyx_gsxz_view():
     return render_template('zwyx_gsxz.html')
+
+
 # 公司性质和薪资
 @app.route('/zwyx/gsxz')
 def show_zwyx_gsxz():
@@ -174,7 +188,7 @@ def show_zwyx_gsxz():
             gsxz_list.append(item[2])
             max_xz.append(item[3])
             min_xz.append(item[4])
-        #数据json化
+        # 数据json化
         returnDate['count_zw'] = count_zw
         returnDate['avg_zw'] = avg_zw
         returnDate['gsxz_list'] = gsxz_list
@@ -182,11 +196,14 @@ def show_zwyx_gsxz():
         returnDate['min_xz'] = min_xz
     return jsonify(returnDate)
 
+
 # 经验薪资图页面（雷达图）
 # 参考链接：http://echarts.baidu.com/demo.html#radar-custom
 @app.route('/zwyx/jy_index')
 def zwyx_jy_view():
     return render_template('zwyx_jy.html')
+
+
 # 经验和薪资
 @app.route('/zwyx/jy')
 def show_zwyx_jy():
@@ -213,10 +230,10 @@ def show_zwyx_jy():
         for item in jy_zwyx_list:
             count_zw.append(item[0])
             avg_zw.append(round(item[1], 2))
-            jy_list.append({'text':item[2]})
+            jy_list.append({'text': item[2]})
             max_xz.append(item[3])
             min_xz.append(item[4])
-        #数据json化
+        # 数据json化
         returnDate['count_zw'] = count_zw
         returnDate['avg_zw'] = avg_zw
         returnDate['jy_list'] = jy_list
@@ -224,11 +241,14 @@ def show_zwyx_jy():
         returnDate['min_xz'] = min_xz
     return jsonify(returnDate)
 
+
 # 职位名称薪资图页面（象形柱图）
 # 参考链接：http://echarts.baidu.com/demo.html#pictorialBar-dotted
 @app.route('/zwyx/zwmc_index')
 def zwyx_zwmc_view():
     return render_template('zwyx_zwmc.html')
+
+
 # 职位名称和薪资
 @app.route('/zwyx/zwmc')
 def show_zwyx_zwmc():
@@ -252,20 +272,20 @@ def show_zwyx_zwmc():
     avg_zw['其他'] = 0
     max_xz['其他'] = 0
     min_xz['其他'] = 0
-    zwmc_zwyx_list=cursor.fetchall()
-    #打开职位分类文本
+    zwmc_zwyx_list = cursor.fetchall()
+    # 打开职位分类文本
     file = open('ca_list.txt', 'r')
     lines = file.readline()
     if zwmc_zwyx_list:
-        #提取数据
+        # 提取数据
         for row in zwmc_zwyx_list:
-            #提取文本数据
+            # 提取文本数据
             for item in lines.split():
-                #判断是否包含
+                # 判断是否包含
                 if item in row[2]:
-                    #如果判断职位列表里是否存在该项
+                    # 如果判断职位列表里是否存在该项
                     if item in zwmc_list:
-                        #存在则处理数据
+                        # 存在则处理数据
                         count_zw[item] = count_zw[item] + row[0]
                         avg_zw[item] = round((avg_zw[item] + row[1]) / 2, 2)
                         max_xz[item] = max(max_xz[item], row[3])
@@ -278,21 +298,21 @@ def show_zwyx_zwmc():
                         max_xz[item] = row[3]
                         min_xz[item] = row[4]
                 else:
-                    #如果没有找到对应的分类，则增加相应数据
+                    # 如果没有找到对应的分类，则增加相应数据
                     count_zw['其他'] = count_zw['其他'] + row[0]
-                    #如果数据还没有对应数值，则增加第一条数据
+                    # 如果数据还没有对应数值，则增加第一条数据
                     if count_zw['其他'] == 0:
                         avg_zw['其他'] = row[1]
                     else:
                         avg_zw['其他'] = round((count_zw['其他'] + row[1]) / 2, 2)
                     max_xz['其他'] = max(max_xz['其他'], row[3])
                     min_xz['其他'] = min(min_xz['其他'], row[4])
-        #重新构建数据
-        return_count_zw=[]
+        # 重新构建数据
+        return_count_zw = []
         return_avg_zw = []
         return_max_xz = []
         return_min_xz = []
-        #重新规划数据
+        # 重新规划数据
         for value in count_zw:
             return_count_zw.append(count_zw[value])
         for value in avg_zw:
@@ -301,7 +321,7 @@ def show_zwyx_zwmc():
             return_max_xz.append(max_xz[value])
         for value in min_xz:
             return_min_xz.append(min_xz[value])
-        #json数据
+        # json数据
         returnDate['status'] = 1
         returnDate['count_zw'] = return_count_zw
         returnDate['avg_zw'] = return_avg_zw
@@ -310,11 +330,14 @@ def show_zwyx_zwmc():
         returnDate['min_xz'] = return_min_xz
     return jsonify(returnDate)
 
-#公司数和地点关系（散点图）
-#参考链接：http://echarts.baidu.com/demo.html#scatter-map-brush
+
+# 公司数和地点关系（散点图）
+# 参考链接：http://echarts.baidu.com/demo.html#scatter-map-brush
 @app.route('/dd/gsmc_index')
 def dd_gsmc_view():
     return render_template('dd_gsmc.html')
+
+
 # 公司数和地点
 @app.route('/dd/gsmc')
 def show_dd_gsmc():
@@ -329,15 +352,18 @@ def show_dd_gsmc():
     if gsmc_dd_list:
         returnDate['status'] = 1
         for item in gsmc_dd_list:
-            count_gs.append({'name':item[1],'value':item[0]})
-        returnDate['count_gs']=count_gs
+            count_gs.append({'name': item[1], 'value': item[0]})
+        returnDate['count_gs'] = count_gs
     return jsonify(returnDate)
 
+
 # 职位数和地点页面（热力图）
-#参考链接：http://echarts.baidu.com/demo.html#heatmap-map
+# 参考链接：http://echarts.baidu.com/demo.html#heatmap-map
 @app.route('/dd/zwmc_index')
 def dd_zwmc_view():
     return render_template('dd_zwmc.html')
+
+
 # 职位名称和薪资
 @app.route('/dd/zwmc')
 def show_dd_zwmc():
@@ -356,23 +382,26 @@ def show_dd_zwmc():
         returnDate['count_zw'] = count_zw
     return jsonify(returnDate)
 
-#职位类型相关关系（综合图）
+
+# 职位类型相关关系（综合图）
 # 参考链接：http://echarts.baidu.com/demo.html#watermark
 @app.route('/dd/type_index')
 def dd_type_view():
     return render_template('dd_type.html')
+
+
 # 公司数和地点
 @app.route('/dd/type')
 def show_dd_type():
     returnDate = {}
     returnDate['status'] = 0
-    #查询职位类型与地点、职位、公司、平均薪资分布总数的关系
+    # 查询职位类型与地点、职位、公司、平均薪资分布总数的关系
     sql = 'SELECT count(distinct ca_list.dd_id),count(distinct ca_list.zwmc_id),count(distinct ca_list.gsmc_id),avg(ca_list.zwyx),ca_type.`name` from ca_list inner join ca_type ON ca_type.Id =ca_list.type_id GROUP BY ca_list.type_id'
     # 执行sql语句
     cursor.execute(sql)
     type_dd_list = cursor.fetchall()
     # 分布地点数
-    dd_type ={}
+    dd_type = {}
     # 分布地点最大数
     count_dd_type = 0
     # 职位数
@@ -383,34 +412,35 @@ def show_dd_type():
     avg_xz = {}
     # 平均薪资最大数
     max_avg_xz = 0
-    #总平均薪资
-    all_avg=0
+    # 总平均薪资
+    all_avg = 0
     returnDate['status'] = 1
-    #遍历数据
+    # 遍历数据
     for item in type_dd_list:
-        dd_type[item[4]]=item[0]
-        count_dd_type=max(count_dd_type,item[0])
-        zw_type[item[4]]=item[1]
-        gs_type[item[4]]=item[2]
-        avg_xz[item[4]]=round(item[3],2)
-        max_avg_xz=max(max_avg_xz,round(item[3],2))
-        if all_avg==0:
-            all_avg=round(item[3],2)
+        dd_type[item[4]] = item[0]
+        count_dd_type = max(count_dd_type, item[0])
+        zw_type[item[4]] = item[1]
+        gs_type[item[4]] = item[2]
+        avg_xz[item[4]] = round(item[3], 2)
+        max_avg_xz = max(max_avg_xz, round(item[3], 2))
+        if all_avg == 0:
+            all_avg = round(item[3], 2)
         else:
-            all_avg=round((all_avg+round(item[3],2))/2,2)
-    #Json化数据
-    returnDate['dd_type']=dd_type
+            all_avg = round((all_avg + round(item[3], 2)) / 2, 2)
+    # Json化数据
+    returnDate['dd_type'] = dd_type
     returnDate['zw_type'] = zw_type
     returnDate['gs_type'] = gs_type
     returnDate['avg_xz'] = avg_xz
-    returnDate['all_avg']=round(all_avg,2)
-    returnDate['count_dd_type'] = count_dd_type+20
-    returnDate['max_avg_xz'] = round(max_avg_xz+1000.0,2)
+    returnDate['all_avg'] = round(all_avg, 2)
+    returnDate['count_dd_type'] = count_dd_type + 20
+    returnDate['max_avg_xz'] = round(max_avg_xz + 1000.0, 2)
     return jsonify(returnDate)
+
 
 # 入口
 if __name__ == '__main__':
     # 调试模式
     app.debug = True
     # 外部可访问的服务器
-    app.run(host='0.0.0.0')
+    app.run()
